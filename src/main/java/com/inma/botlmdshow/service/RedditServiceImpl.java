e951ac7109c6185bfc0fd39731679153c5a9c556
Adrian Marin <adrianmarinportillo@gmail.com>
2022-03-05 17:47:19 +0100

    Simple log

--- src/main/java/com/inma/botlmdshow/service/RedditServiceImpl.java
+++ src/main/java/com/inma/botlmdshow/service/RedditServiceImpl.java
@@ -4,6 +4,8 @@ import com.inma.botlmdshow.domain.PostDTO;
 import com.inma.botlmdshow.domain.PostDTOAdapter;
 import com.inma.botlmdshow.service.reddit.PushshiftService;
 import com.inma.botlmdshow.service.reddit.RedditApiService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,7 +16,7 @@ public class RedditServiceImpl implements RedditService {
 
     private final PushshiftService pushShiftService;
     private final RedditApiService redditApiService;
-
+    private static final Logger log = LoggerFactory.getLogger(RedditServiceImpl.class);
 
     public RedditServiceImpl(PushshiftService pushshiftService, RedditApiService redditApiService) {
         this.pushShiftService = pushshiftService;
@@ -25,8 +27,10 @@ public class RedditServiceImpl implements RedditService {
     public List<PostDTO> getAllPosts() {
         Optional<List<PostDTO>> postDTOS = pushShiftService.getAllPosts().map(PostDTOAdapter::adaptFromPushshift);
         if (postDTOS.isPresent()) {
+            log.info("API de Pushshift devolvió un codigo 200");
             return postDTOS.get();
         }
+        log.info("Utilizando API de Reddit.");
         Optional<List<PostDTO>> redditDTO = redditApiService.getAllPosts().map(PostDTOAdapter::adaptFromRedditJson);
         return redditDTO.orElseThrow();
     }
